8a1fc71d3c92180f68262f8d31df50e6f43a1069
Henning Horn <hh@henninghorn.dk>
2016-03-15 16:53:00 +0100

    Added login logic to header component

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -23,10 +23,7 @@
         <div class="header-right header-menu">
           <template v-if="!isAuthenticated">
             <span class="header-item">
-              <a v-link="'create-profile'">Create profile</a>
-            </span>
-            <span class="header-item">
-              <a class="button is-info is-outlined" v-link="'login'">Log in</a>
+              <a class="button is-info is-outlined" @click.prevent="login">Log in</a>
             </span>
           </template>
           <template v-else>
@@ -34,7 +31,7 @@
               <img :src="user.image" class="image is-24x24">&nbsp; {{ user.name }}
             </a>
             <span class="header-item">
-              <a class="button is-info is-outlined" @click.prevent="logout()">Log out</a>
+              <a class="button is-info is-outlined" @click.prevent="logout">Log out</a>
             </span>
           </template>
         </div>
@@ -67,10 +64,26 @@ export default {
           this.user.name = user.facebook.displayName
           this.user.image = user.facebook.profileImageURL
         }
+
+        // Api.createUser({
+        //   email: fbUser.email,
+        //   timezone: 'Europe/Copenhagen',
+        //   first_name: fbUser.first_name,
+        //   last_name: fbUser.last_name
+        // })
+        // .then(response => {
+        //   timekitRef.set(response.data)
+        // })
+        // .catch(error => {
+        //   console.log('timekit error', error)
+        // })
       }
     })
   },
   methods: {
+    login: function () {
+      Auth.loginFacebook()
+    },
     logout: function () {
       Auth.logout()
       this.$route.router.go('/')
