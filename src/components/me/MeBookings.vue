0ca47b489a1452603cd0fe3fc3debf648ba4f880
Henning Horn <hh@henninghorn.dk>
2016-03-16 13:10:46 +0100

    Stuff

--- src/components/me/MeBookings.vue
+++ src/components/me/MeBookings.vue
@@ -1,5 +1,57 @@
 <template>
   <div>
-    AWESOME BOOKINGS
+    <table class="table is-striped">
+      <thead>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Email
+          </th>
+          <th>
+            Space
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="booking in bookings">
+          <td>
+            <template v-for="customer in booking.customers">
+              {{ customer.name }}
+            </template>
+          </td>
+          <td>
+            <template v-for="customer in booking.customers">
+              {{ customer.email }}
+            </template>
+          </td>
+          <td>
+            .
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import Api from '../../services/Api'
+
+export default {
+  data () {
+    return {
+      bookings: []
+    }
+  },
+  ready: function () {
+    Api.getBookings()
+    .then(response => {
+      this.bookings = response.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+}
+</script>
